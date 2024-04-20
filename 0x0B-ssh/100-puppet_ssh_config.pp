f0206c83214734f0965f2632c43376d17a264684
nadazkai21 <nadazaki019@gmail.com>
2024-04-21 01:06:20 +0200

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,24 +1,24 @@
 # Your SSH client configuration must be configured to refuse to authenticate using a password
 # Your SSH client configuration must be configured to use the private key ~/.ssh/school
 
-include stdlib
+
 
 file_line { 'config_file_pass':
-        ensure  => 'file',
+        ensure  => 'present',
         path    => '/etc/ssh/ssh_config',
         line   => '   PasswordAuthentication no',
 }
 
 file_line { 'config_file_key':
         
-        ensure  => 'file',
+        ensure  => 'present',
         path    => '/etc/ssh/ssh_config',
         line   => '   IdentityFile ~/.ssh/school',
 }
 
 
 file_line { 'remove_pass':
-      ensure            => 'file',
+      ensure            => 'present',
       path              => '/etc/ssh/ssh_config',
       match             => '   PasswordAuthentication no',
       match_for_absence => true,
