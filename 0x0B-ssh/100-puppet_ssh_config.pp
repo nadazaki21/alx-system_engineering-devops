fc122aa7f1039b379f508ac2f6af5ee3d96a4571
nadazkai21 <nadazaki019@gmail.com>
2024-04-20 15:19:10 +0200

    3

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,5 +5,5 @@ file { 'config_file':
         
         path    => '/etc/ssh/ssh_config',
         ensure  => 'file',
-        content =>  file('./config_file'),
+        content =>  file('config_file'),
 }
