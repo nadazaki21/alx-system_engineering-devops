3549eaf87e9ba417ddd9a19f723344cd9ef56896
nadazkai21 <nadazaki019@gmail.com>
2024-04-24 03:09:48 +0200

    7

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+# manifest to install and configure nginx
+
+package { 'nginx':
+    name     => 'nginx',
+    provider => 'apt',
+    ensure   => 'installed',
+}
+
+file { 'default-page':
+    path    => '/var/www/html/index.nginx-debian.html',
+    ensure  => 'present',
+    content => 'Hello World!',
+}
+
+file_line { 'redirection':
+    path  => '/etc/nginx/sites-available/default',
+    line  => ' location /redirect_me {\nreturn 301 permanent;\n    }',
+    after => '^server_name',
+}
