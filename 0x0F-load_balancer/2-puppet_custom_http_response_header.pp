fd65fb6295a51a4fff84fa4929488f1d42544fcd
nadazkai21 <nadazaki019@gmail.com>
2024-04-30 01:56:39 +0300

    1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,27 +1,24 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
 
 exec { 'update':
-
     command => '/usr/bin/apt update',
 }
 
-
 package { 'install_nginx':
-
-    name    => 'nginx',
+    name     => 'nginx',
     provider => 'apt',
+    require  => Exec['update'], # Ensure 'update' command is executed before installing nginx
 }
 
-
 exec { 'insert line':
-
-    command => '/usr/bin/sed -i \'57i\add_header X-Served-By $HOSTNAME always;\' /etc/nginx/nginx.conf ',
-    environment => ['HOSTNAME=$HOSTNAME'],
+    command     => '/usr/bin/sed -i \'57iadd_header X-Served-By $HOSTNAME always;\' /etc/nginx/nginx.conf',
+    environment => ['HOSTNAME=myhostname'], # Set the HOSTNAME environment variable
+    require     => Package['install_nginx'], # Ensure nginx is installed before modifying its config
 }
 
 exec { 'reload_nginx':
-
-    command => '/etc/init.d/nginx reload',
-    environment => ['HOSTNAME=$HOSTNAME'],
+    command     => '/etc/init.d/nginx reload',
+    require     => Exec['insert line'], # Ensure 'insert line' command is executed before reloading nginx
 }
+
 
