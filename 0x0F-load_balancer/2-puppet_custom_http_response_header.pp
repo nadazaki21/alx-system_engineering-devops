3fd5659268a6e96a65448187b168bdf3116cf7de
nadazkai21 <nadazaki019@gmail.com>
2024-04-30 02:24:50 +0300

    1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -15,8 +15,8 @@ package { 'nginx':
 
 exec { 'insert line':
 
-    command => '/usr/bin/sed -i "65i\add_header X-Served-By $HOSTNAME always;" /etc/nginx/sites-available/default',
-    environment => ['HOSTNAME=$HOSTNAME'],
+    command => '/usr/bin/sed -i "65i\add_header X-Served-By ${hostname} always;" /etc/nginx/sites-available/default',
+    
 }
 
 exec {'start_server':
