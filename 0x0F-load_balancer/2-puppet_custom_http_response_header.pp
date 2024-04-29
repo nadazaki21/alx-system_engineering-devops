3d19815293a35dbe3528ab9362dde1156cfc616f
nadazkai21 <nadazaki019@gmail.com>
2024-04-30 02:36:30 +0300

    1

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,10 +1,5 @@
 # automate the task of creating a custom HTTP header response, but with Puppet.
 
-exec { 'update':
-
-    command => '/usr/bin/sudo /usr/bin/apt update',
-}
-
 
 package { 'nginx':
     name     => 'nginx',
@@ -15,12 +10,13 @@ package { 'nginx':
 
 exec { 'insert line':
 
-    command => ' /usr/bin/sudo /usr/bin/sed -i "65i\add_header X-Served-By ${hostname} always;" /etc/nginx/sites-available/default',
-    
+    command => '/usr/bin/sed -i "65i\add_header X-Served-By ${hostname} always;" /etc/nginx/sites-available/default',
+
 }
 
 exec {'start_server':
 
-command => '/usr/bin/sudo /usr/sbin/service nginx restart',
+    command => '/usr/bin/sudo /usr/sbin/service nginx restart',
+
 }
 
